52e4fdd38e8ba2a79a0e7a2a0faeb95b0adcc9af
Aryan Patil <aryanpatil@Aryans-MacBook-Air.local>
2025-06-20 22:20:04 +0530

    Cards developed, need slight adjustments

---
+++ supply chain management system/src/Pages/Components/Landing Page/Features.jsx
@@ -0,0 +1,31 @@
+//The features in the homepage include Card Components
+// import BlueBackground from './assets/blue.jpeg';
+import './Features.css'
+
+function Features(){
+    return(
+        <>
+        <div className="Features">
+            <div id="EcoFriendly">
+                <h3 id="EcoHeader">ECO FRIENDLY SHIPPING</h3>
+                <p id="EcoPara">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate hic omnis placeat voluptatem est architecto</p>
+                <button id="EcoButton">EXPLORE</button>
+            </div>
+
+            <div id="SpeedyService">
+                <h3 id="SpeedyHeader">SPEEDY SERVICE</h3>
+                <p id="SpeedyPara">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quisquam porro assumenda eius laudantium. Omnis.</p>
+                <button id="SpeedyButton">EXPLORE</button>
+            </div>
+
+            <div id="OrderAndShipmentTracking">
+                <h3 id="OrderHeader">ORDER AND SHIPMENT TRACKING</h3>
+                <p id="OrderPara">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt soluta voluptates, hic atque minima beatae.</p>
+                <button id="OrderButton">EXPLORE</button>
+            </div>
+            </div>
+            </>
+    );
+}
+
+export default Features;
---
+++ supply chain management system/src/Pages/Components/Landing Page/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header(){
+
+    return(
+        <header>
+            <nav>
+                <ul></ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Orders</a></li>
+                <li><a href="#">Ship Now</a></li>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
