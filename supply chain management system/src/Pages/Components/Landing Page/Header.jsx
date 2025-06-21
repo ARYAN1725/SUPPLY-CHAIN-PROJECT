904d31a4c4b02b989978a81dd1549558152faf7a
Aryan Patil <aryanpatil@Aryans-MacBook-Air.local>
2025-06-21 11:28:15 +0530

    Created navbar, minor changes required

--- supply chain management system/src/Pages/Components/Landing Page/Header.jsx
+++ supply chain management system/src/Pages/Components/Landing Page/Header.jsx
@@ -1,14 +1,20 @@
+import './Header.css';
 
 function Header(){
 
     return(
         <header>
-            <nav>
-                <ul></ul>
-                <li><a href="#">Home</a></li>
+            <nav className="navbar">
+                <div className="header-image">
+                <img src="/Logo.jpg" id="Logo" />
+                <ul>
+                <li><a href="#" id="HomeElement">Home</a></li>
                 <li><a href="#">About Us</a></li>
                 <li><a href="#">Orders</a></li>
                 <li><a href="#">Ship Now</a></li>
+                <button id="LoginButton">LOGIN</button>
+                </ul>
+                </div>
             </nav>
         </header>
     );
